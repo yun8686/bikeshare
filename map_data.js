08f7ddffde2efca15d7d3308fafe6cca55836ed1
a.aguri <>
2019-06-02 22:00:38 +0900

    座標取得の修正

--- map_data.js
+++ map_data.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const URL = 'https://www.google.com/maps/d/u/0/viewer?mid=1L2l1EnQJhCNlm_Xxkp9RTjIj68Q&ll';
 (async() => {
-    const browser = await puppeteer.launch({ headless: ture });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(URL), {waitUntil: "domcontentloaded"};
 
@@ -20,39 +20,49 @@ const URL = 'https://www.google.com/maps/d/u/0/viewer?mid=1L2l1EnQJhCNlm_Xxkp9RT
     const baseLength = (await page.$$('.pbTTYe-ibnC6b-Bz112c')).length
     for(var i = 0; i < baseLength; i++){
 
-        // 対象のリンクをクリック
-        await positonBase[i].click();
+      // オブジェクトを作成する
+      let data_obj = new Object();
 
-        // 画像パスを取得
-        var image = await page.$eval('.pbTTYe-ibnC6b-Bz112c > img', item => {
-            return item.getAttribute('src');
-        });
-        console.log(image);
-    
-        // 名前取得
-        var nameText = await page.$eval('.qqvbed-p83tee-lTBxed', item => {
-            return item.textContent;
-        });
-        console.log(nameText);
+      // 対象のリンクをクリック
+      await positonBase[i].click();
 
-        // 緯度と経度
-        let position = page.url().match(/(\d+\.\d+)/g);
-        console.log(position[0]);
-        console.log(position[1]);
+      // 画像パスを取得
+      var image = await page.$eval('.pbTTYe-ibnC6b-Bz112c > img', item => {
+          return item.getAttribute('src');
+      });
+      // console.log(image);
+      data_obj.image = image;
+  
+      // 名前取得
+      var nameText = await page.$eval('.qqvbed-p83tee-lTBxed', item => {
+          return item.textContent;
+      });
+      // console.log(nameText);
+      data_obj.name = nameText;
 
-        // サブウィンドウを閉じる処理
-        await page.waitFor(500);
-        const buttonSelectors = await page.$$('.HzV7m-tJHJj-LgbsSe-haAclf.qqvbed-a4fUwd-LgbsSe-haAclf');
-        for (const buttonSelector of buttonSelectors) {
-            const button = await buttonSelector.$('span.Ce1Y1c');
-            const buttonHTML = await buttonSelector.getProperty('innerHTML');
-            const buttonValue = await buttonHTML.jsonValue();
-            if (buttonValue.indexOf('aria-label="戻る"') !== -1) {
-                await button.click();
-                break;
-            }
-        }
-        await page.waitFor(500);
+
+      // サブウィンドウを閉じる処理
+      await page.waitFor(500);
+      // 緯度と経度
+      let position = await page.url().match(/(\d+\.\d+)/g);
+      // console.log(position[0]);
+      // console.log(position[1]);
+      data_obj.longitude = position[0];
+      data_obj.latitude = position[1];
+      
+      const buttonSelectors = await page.$$('.HzV7m-tJHJj-LgbsSe-haAclf.qqvbed-a4fUwd-LgbsSe-haAclf');
+      for (const buttonSelector of buttonSelectors) {
+          const button = await buttonSelector.$('span.Ce1Y1c');
+          const buttonHTML = await buttonSelector.getProperty('innerHTML');
+          const buttonValue = await buttonHTML.jsonValue();
+          if (buttonValue.indexOf('aria-label="戻る"') !== -1) {
+              await button.click();
+              break;
+          }
+      }
+      console.log(data_obj);
+      console.log(',');
+      await page.waitFor(500);
     }
     browser.close();
 })();
